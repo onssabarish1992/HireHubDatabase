264853e6003788a45af6b08c76ffe828264a6657
RishabhChauhan <104836337+RishabhChauhan@users.noreply.github.com>
2022-05-14 23:25:46 +0100

    Update AspNetRoles.sql
    Changed DB Schema

--- Tables/AspNetRoles.sql
+++ Tables/AspNetRoles.sql
@@ -1,8 +1,9 @@
-﻿CREATE TABLE [dbo].[AspNetRoles]
-(
-	[Id] [nvarchar](450) NOT NULL,
-	[Name] [nvarchar](256) NULL,
-	[NormalizedName] [nvarchar](256) NULL,
-	[ConcurrencyStamp] [nvarchar](max) NULL,
-	CONSTRAINT [PK_AspNetRoles] PRIMARY KEY CLUSTERED ([Id] ASC)
+﻿CREATE TABLE [dbo].[AspNetRoles](
+[Id] [nvarchar](450) NOT NULL,
+[Name] [nvarchar](256) NULL,
+[NormalizedName] [nvarchar](256) NULL,
+[ConcurrencyStamp] [nvarchar](max) NULL,
+CONSTRAINT [PK_AspNetRoles] PRIMARY KEY CLUSTERED
+
+([Id] ASC)
 )
