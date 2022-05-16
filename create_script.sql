f06d09b9ee5e9627c72c484d44c9e3764d895b35
KishoreValavan <47786480+KishoreValavan@users.noreply.github.com>
2022-05-17 00:22:00 +0100

    Update and rename create_script to create_script.sql

--- create_script.sql
+++ create_script.sql
@@ -1,3 +1,4 @@
+-- create table script
 CREATE TABLE tbl_EmployeeMaster (
 	emp_id INT NOT NULL IDENTITY(1, 1) PRIMARY KEY
 	,emp_code VARCHAR(50) NOT NULL
@@ -22,17 +23,17 @@ CREATE TABLE tbl_JobMaster (
 CREATE TABLE tbl_CriteriaMaster (
 	criteria_id INT NOT NULL IDENTITY(1, 1) PRIMARY KEY
 	,criteria_name VARCHAR(50) NOT NULL
-	,job_description VARCHAR(250) NOT NULL
+	,criteria_description VARCHAR(250) NOT NULL
 	,date_created DATETIME NOT NULL DEFAULT GETDATE()
 	,created_by INT NOT NULL
 	,date_modified DATETIME DEFAULT GETDATE()
 	,modified_by INT
 	);
 
-CREATE TABLE tbl_JobCriteriaMaping (
+CREATE TABLE tbl_JobCriteriaMapping (
 	job_criteria_mapping_id INT NOT NULL IDENTITY(1, 1) PRIMARY KEY
 	,job_id INT NOT NULL FOREIGN KEY REFERENCES tbl_JobMaster(job_id)
-	,criteria_id INT NOT NULL FOREIGN KEY REFERENCES tbl_ CriteriaMaster(criteria_id)
+	,criteria_id INT NOT NULL FOREIGN KEY REFERENCES tbl_CriteriaMaster(criteria_id)
 	,date_created DATETIME NOT NULL DEFAULT GETDATE()
 	,created_by INT NOT NULL
 	,date_modified DATETIME DEFAULT GETDATE()
@@ -42,7 +43,7 @@ CREATE TABLE tbl_JobCriteriaMaping (
 CREATE TABLE tbl_CandidateDetails (
 	Candidate_details_id INT NOT NULL IDENTITY(1, 1) PRIMARY KEY
 	,job_id INT NOT NULL FOREIGN KEY REFERENCES tbl_JobMaster(job_id)
-	,criteria_id INT NOT NULL FOREIGN KEY REFERENCES tbl_ CriteriaMaster(criteria_id)
+	,criteria_id INT NOT NULL FOREIGN KEY REFERENCES tbl_CriteriaMaster(criteria_id)
 	,project_name VARCHAR(100)
 	,date_created DATETIME NOT NULL DEFAULT GETDATE()
 	,created_by INT NOT NULL
@@ -52,7 +53,7 @@ CREATE TABLE tbl_CandidateDetails (
 
 CREATE TABLE tbl_CandidateInterviewSchedule (
 	schedule_id INT NOT NULL IDENTITY(1, 1) PRIMARY KEY
-	,candidate_id INT NOT NULL FOREIGN KEY REFERENCES tbl_CandidateDetails(candidate_id)
+	,candidate_id INT NOT NULL FOREIGN KEY REFERENCES tbl_CandidateDetails(Candidate_details_id)
 	,job_id INT NOT NULL FOREIGN KEY REFERENCES tbl_JobMaster(job_id)
 	,interviewer_id INT NOT NULL FOREIGN KEY REFERENCES tbl_EmployeeMaster(emp_id)
 	,interview_timestamp DATETIME NOT NULL
@@ -65,7 +66,7 @@ CREATE TABLE tbl_CandidateInterviewSchedule (
 CREATE TABLE tbl_CandidateEvaluation (
 	evaluation_id INT NOT NULL IDENTITY(1, 1) PRIMARY KEY
 	,schedule_id INT NOT NULL FOREIGN KEY REFERENCES tbl_CandidateInterviewSchedule(schedule_id)
-	,job_criteria_mapping_id INT NOT NULL FOREIGN KEY REFERENCES tbl_JobCriteriaMaping(job_criteria_mapping_id)
+	,job_criteria_mapping_id INT NOT NULL FOREIGN KEY REFERENCES tbl_JobCriteriaMapping(job_criteria_mapping_id)
 	,criteria_score INT NOT NULL
 	,date_created DATETIME NOT NULL DEFAULT GETDATE()
 	,created_by INT NOT NULL
@@ -75,7 +76,7 @@ CREATE TABLE tbl_CandidateEvaluation (
 
 CREATE TABLE tbl_CandidateGlobalScores (
 	global_score_id INT NOT NULL IDENTITY(1, 1) PRIMARY KEY
-	,candidate_id INT NOT NULL FOREIGN KEY REFERENCES tbl_CandidateDetails(candidate_id)
+	,candidate_id INT NOT NULL FOREIGN KEY REFERENCES tbl_CandidateDetails(Candidate_details_id)
 	,schedule_id INT NOT NULL FOREIGN KEY REFERENCES tbl_CandidateInterviewSchedule(schedule_id)
 	,job_id INT NOT NULL FOREIGN KEY REFERENCES tbl_JobMaster(job_id)
 	,is_hired BIT NOT NULL
