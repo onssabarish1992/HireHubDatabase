7db670f488f7f295efbe7457fdcd78d8e74cb6e3
KishoreValavan <47786480+KishoreValavan@users.noreply.github.com>
2022-05-18 12:15:27 +0100

    Create tbl_JobCriteriaMapping.sql

---
+++ Tables/tbl_JobCriteriaMapping.sql
@@ -0,0 +1,9 @@
+CREATE TABLE tbl_JobCriteriaMapping (
+	job_criteria_mapping_id INT NOT NULL IDENTITY(1, 1) PRIMARY KEY
+	,job_id INT NOT NULL FOREIGN KEY REFERENCES tbl_JobMaster(job_id)
+	,criteria_id INT NOT NULL FOREIGN KEY REFERENCES tbl_CriteriaMaster(criteria_id)
+	,date_created DATETIME NOT NULL DEFAULT GETDATE()
+	,created_by NVARCHAR(450) NOT NULL
+	,date_modified DATETIME DEFAULT GETDATE()
+	,modified_by NVARCHAR(450)
+	);
