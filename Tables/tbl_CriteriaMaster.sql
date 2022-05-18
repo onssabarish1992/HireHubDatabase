c11ed300822517252eba4591e456b4411402ab8f
KishoreValavan <47786480+KishoreValavan@users.noreply.github.com>
2022-05-18 12:14:22 +0100

    Update tbl_CriteriaMaster.sql

--- Tables/tbl_CriteriaMaster.sql
+++ Tables/tbl_CriteriaMaster.sql
@@ -3,7 +3,7 @@ CREATE TABLE tbl_CriteriaMaster (
 	,criteria_name VARCHAR(50) NOT NULL
 	,criteria_description VARCHAR(250) NOT NULL
 	,date_created DATETIME NOT NULL DEFAULT GETDATE()
-	,created_by INT NOT NULL
+	,created_by NVARCHAR(450) NOT NULL
 	,date_modified DATETIME DEFAULT GETDATE()
-	,modified_by INT
+	,modified_by NVARCHAR(450)
 	);
