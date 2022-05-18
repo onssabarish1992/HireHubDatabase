8e91e1b41c1060d0a1c9f2f8993b9d0ff90055eb
KishoreValavan <47786480+KishoreValavan@users.noreply.github.com>
2022-05-18 12:11:14 +0100

    Create tbl_EmployeeMaster.sql

---
+++ Tables/tbl_EmployeeMaster.sql
@@ -0,0 +1,10 @@
+CREATE TABLE tbl_EmployeeMaster (
+	emp_id INT NOT NULL IDENTITY(1, 1) PRIMARY KEY
+	,emp_code VARCHAR(50) NOT NULL
+	,emp_name VARCHAR(255) NOT NULL
+	,emp_designation VARCHAR(20) NOT NULL
+	,date_created DATETIME NOT NULL DEFAULT GETDATE()
+	,created_by INT NOT NULL
+	,date_modified DATETIME DEFAULT GETDATE()
+	,modified_by INT
+	);
