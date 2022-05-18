2a82e5203baef75cf765c96de3cc1a1a71060c63
KishoreValavan <47786480+KishoreValavan@users.noreply.github.com>
2022-05-18 12:11:48 +0100

    Create tbl_CriteriaMaster.sql

---
+++ Tables/tbl_CriteriaMaster.sql
@@ -0,0 +1,9 @@
+CREATE TABLE tbl_CriteriaMaster (
+	criteria_id INT NOT NULL IDENTITY(1, 1) PRIMARY KEY
+	,criteria_name VARCHAR(50) NOT NULL
+	,criteria_description VARCHAR(250) NOT NULL
+	,date_created DATETIME NOT NULL DEFAULT GETDATE()
+	,created_by INT NOT NULL
+	,date_modified DATETIME DEFAULT GETDATE()
+	,modified_by INT
+	);
