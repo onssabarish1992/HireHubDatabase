ede5fe05aa4cb383bbb3bf32d233a0b3fa01e804
KishoreValavan <47786480+KishoreValavan@users.noreply.github.com>
2022-05-18 12:13:23 +0100

    Update tbl_CandidateDetails.sql

--- Tables/tbl_CandidateDetails.sql
+++ Tables/tbl_CandidateDetails.sql
@@ -1,9 +1,10 @@
-﻿CREATE TABLE [dbo].[tbl_CandidateDetails]
-(
-	 candidate_id INT NOT NULL IDENTITY(1, 1) PRIMARY KEY
-	,candidate_name VARCHAR(250) NOT NULL
+CREATE TABLE tbl_CandidateDetails (
+	Candidate_details_id INT NOT NULL IDENTITY(1, 1) PRIMARY KEY
+	,job_id INT NOT NULL FOREIGN KEY REFERENCES tbl_JobMaster(job_id)
+	,criteria_id INT NOT NULL FOREIGN KEY REFERENCES tbl_CriteriaMaster(criteria_id)
+	,project_name VARCHAR(100)
 	,date_created DATETIME NOT NULL DEFAULT GETDATE()
 	,created_by NVARCHAR(450) NOT NULL
-	,date_modified DATETIME 
+	,date_modified DATETIME DEFAULT GETDATE()
 	,modified_by NVARCHAR(450)
-)
+	);
