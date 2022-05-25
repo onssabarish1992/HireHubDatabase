a0b3bf2bf1767961f64f2e3a88c22e688c46910b
RishabhChauhan <104836337+RishabhChauhan@users.noreply.github.com>
2022-05-25 15:41:16 +0100

    Create usp_sav_SubCriteria.sql
    insert details into Sub Criteria new table

---
+++ Stored Procedures/usp_sav_SubCriteria.sql
@@ -0,0 +1,71 @@
+﻿-- =============================================
+-- Author: Rishabh Singh Chauhan
+-- Create date: 22-May-2022
+-- Description: This stored procedure is used to insert details into Sub Criteria new table
+-- =============================================
+CREATE PROCEDURE [dbo].[usp_sav_SubCriteria]
+@job_id INT,
+@criteria_id INT,
+    @sub_criteria_description VARCHAR(450),
+    @CreatedBy NVARCHAR(450)
+AS
+BEGIN
+BEGIN TRY
+SET NOCOUNT ON;
+
+DECLARE @ErrorSeverity INT
+,@ErrorState INT
+,@StackTrace NVARCHAR(MAX)
+,@Params NVARCHAR(MAX);
+
+
+INSERT INTO [dbo].[tbl_SubCriteria]
+  ([job_id]
+  ,[criteria_id]
+  ,[sub_criteria_description]
+  ,[weightage]
+  ,[is_Active]
+  ,[date_created]
+  ,[created_by]
+  ,[date_modified]
+  ,[modified_by])
+VALUES
+  (@job_id
+  ,@criteria_id
+  ,@sub_criteria_description
+  ,NULL
+  ,1
+  ,GETDATE()
+  ,@CreatedBy
+  ,NULL
+  ,NULL)
+
+END TRY
+
+BEGIN CATCH
+SET @ErrorSeverity = ERROR_SEVERITY();
+SET @ErrorState = ERROR_STATE();
+SET @StackTrace = ERROR_MESSAGE();
+
+IF (ERROR_NUMBER() = 50000) -- User defined error
+BEGIN
+RAISERROR (
+@StackTrace
+,@ErrorSeverity
+,@ErrorState
+);
+END
+ELSE
+BEGIN
+
+SET @Params= '@job_id ='+CONVERT(NVARCHAR(MAX),@job_id)+
+' | @criteria_id ='+CONVERT(NVARCHAR(MAX),@criteria_id)+
+' | @sub_criteria_description ='+CONVERT(NVARCHAR(MAX),@sub_criteria_description)+
+' | @CreatedBy ='+CONVERT(NVARCHAR(MAX),@CreatedBy);
+-- Save error to log table
+EXEC usp_sav_ErrorLog @StackTrace
+,'usp_sav_SubCriteria'
+,@Params;
+END
+END CATCH
+END
