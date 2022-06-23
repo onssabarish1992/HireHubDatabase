bab7165628d3372434272616cb7d74c2f599fa99
Kishore Valavan R <47786480+KishoreValavan@users.noreply.github.com>
2022-06-23 18:22:32 +0100

    Update delete_script.sql

--- Scripts/delete_script.sql
+++ Scripts/delete_script.sql
@@ -1,8 +1,11 @@
+DELETE FROM tbl_CandidateEvaluation
 DELETE FROM tbl_CandidateInterviewSchedule
 DELETE FROM tbl_CandidateDetails
-DELETE FROM tbl_CandidateEvaluation
 DELETE FROM tbl_CandidateGlobalScores
 DELETE FROM tbl_JobCriteria
 DELETE FROM tbl_JobCriteriaMapping
 DELETE FROM Tbl_ErrorLogs
 DELETE FROM tbl_subcriteria
+-- DELETE FROM tbl_JobCriteriaMapping
+-- DELETE FROM tbl_JobMaster
+-- DELETE FROM tbl_CriteriaMaster
