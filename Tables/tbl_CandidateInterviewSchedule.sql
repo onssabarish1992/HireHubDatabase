a1e7b85e1c4ce027da8c2852bcc3c89bf21ee603
Sabarish Nair <121101278@umail.ucc.ie>
2022-07-12 23:49:45 +0100

    Code Changes
    Code Changes

--- Tables/tbl_CandidateInterviewSchedule.sql
+++ Tables/tbl_CandidateInterviewSchedule.sql
@@ -2,7 +2,7 @@ CREATE TABLE tbl_CandidateInterviewSchedule (
 	schedule_id INT NOT NULL IDENTITY(1, 1) PRIMARY KEY
 	,candidate_id INT NOT NULL FOREIGN KEY REFERENCES tbl_CandidateDetails(candidate_id)
 	,job_id INT NOT NULL FOREIGN KEY REFERENCES tbl_JobMaster(job_id)
-	,interviewer_id INT NOT NULL
+	,interviewer_id NVARCHAR(450) NOT NULL
 	,project_name varchar(100) NULL
 	,interview_timestamp DATETIME NOT NULL
 	,date_created DATETIME NOT NULL DEFAULT GETDATE()
--- Tables/tbl_JobMaster.sql
+++ Tables/tbl_JobMaster.sql
@@ -2,7 +2,7 @@
 (
 	 job_id INT NOT NULL IDENTITY(1, 1) PRIMARY KEY
 	,job_name VARCHAR(50) NOT NULL
-	,job_description VARCHAR(250) NOT NULL
+	,job_description VARCHAR(MAX) NOT NULL
 	,date_created DATETIME NOT NULL DEFAULT GETDATE()
 	,created_by NVARCHAR(450) NOT NULL
 	,date_modified DATETIME 
